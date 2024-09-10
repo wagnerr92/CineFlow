5d5c130fcfaf7d049fe5fbaaa6205a6dca4cf2ad
Eduardo <eduardo07assis@gmail.com>
2024-09-10 17:23:06 -0300

    Create initial register screen

---
+++ CineFlow/RegisterSreen/RegisterVC.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterVC.swift
+//  CineFlow
+//
+//  Created by Eduardo on 10/09/24.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
