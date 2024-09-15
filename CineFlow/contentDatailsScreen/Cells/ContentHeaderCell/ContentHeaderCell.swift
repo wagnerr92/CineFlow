cb82a5fbb05e70c16517d45d6cfcc98a7ba45a67
Eduardo <eduardo07assis@gmail.com>
2024-09-15 13:26:14 -0300

    Adding ContentHeaderCell tableview cell components.

---
+++ CineFlow/contentDatailsScreen/Cells/ContentHeaderCell/ContentHeaderCell.swift
@@ -0,0 +1,26 @@
+//
+//  ContentHeaderCell.swift
+//  CineFlow
+//
+//  Created by Eduardo on 15/09/24.
+//
+
+import UIKit
+
+class ContentHeaderCell: UITableViewCell {
+    
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var playerButton: UIButton!
+    @IBOutlet weak var StarImage: UIImageView!
+    
+    
+    @IBOutlet weak var pointsLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
+    
+
