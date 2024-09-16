df60399ac019c2318856bb0e7d27006f6769afcc
Wagner Rodrigues <wagner92dev@gmail.com>
2024-09-16 01:24:21 -0300

    Recover password screen

--- CineFlow/Feature/RecoverPassword/RecoverPasswordViewController.swift
+++ CineFlow/Feature/RecoverPassword/RecoverPasswordViewController.swift
@@ -4,26 +4,80 @@
 //
 //  Created by Wagner Rodrigues on 16/09/24.
 //
-
 import UIKit
 
 class RecoverPasswordViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = CFColor.backgroundDefault
+        
+        configureTextField(emailTextField, placeholder: "Digite o seu e-mail", imageName: "mailIcon")
+        emailTextField.delegate = self
+        
+        // Iniciar o botão desabilitado
+        sendButton.isEnabled = false
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureTextField(_ textField: UITextField, placeholder: String, imageName: String) {
+        textField.backgroundColor = .darkGray
+        textField.layer.cornerRadius = 8.0
+        textField.layer.borderColor = UIColor.black.cgColor
+        textField.layer.borderWidth = 1.0
+        textField.clipsToBounds = true
+        textField.placeholder = placeholder
+        textField.textColor = .black
+        
+        if let image = UIImage(named: imageName) {
+            let imageView = UIImageView(image: image)
+            imageView.contentMode = .scaleAspectFit
+            
+            let iconSize: CGFloat = 15
+            let padding: CGFloat = 8
+            let containerWidth = iconSize + padding * 2
+            let containerHeight = textField.frame.height
+            
+            let containerView = UIView(frame: CGRect(x: 0, y: 0, width: containerWidth, height: containerHeight))
+            imageView.frame = CGRect(x: padding, y: (containerHeight - iconSize) / 2, width: iconSize, height: iconSize)
+            containerView.addSubview(imageView)
+            
+            textField.leftView = containerView
+            textField.leftViewMode = .always
+        }
     }
-    */
+    
+    private func updateEnterButtonState() {
+        // Verificar se o campo de texto contém o caractere "@".
+        if let emailText = emailTextField.text, emailText.contains("@") {
+            sendButton.isEnabled = true
+        } else {
+            sendButton.isEnabled = false
+        }
+    }
 
+    @IBAction func sendButtonTapped(_ sender: Any) {
+        print("implementar")
+    }
+}
+
+extension RecoverPasswordViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        DispatchQueue.main.async {
+            self.updateEnterButtonState()
+        }
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.layer.borderColor = UIColor.blue.cgColor
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.layer.borderColor = UIColor.black.cgColor
+        updateEnterButtonState()
+    }
 }
