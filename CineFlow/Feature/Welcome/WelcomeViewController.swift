384dab99ab84dbe4b7893b09f826b00475017340
Thaís Gomes <thaisgomes.iosmac@gmail.com>
2024-09-17 18:45:22 -0300

    Create Welcome Screen.

---
+++ CineFlow/Feature/Welcome/WelcomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  WelcomeViewController.swift
+//  CineFlow
+//
+//  Created by Thaís Gomes on 12/09/24.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    @IBOutlet weak var letsgoButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBAction func tappedletsgoButton(_ sender: Any) {
+        // let login = UIStoryboard(name: "Tela02ViewController", bundle: nil).instantiateViewController(withIdentifier: "Tela02ViewController") as? Tela02ViewController
+        
+        //MARK: NavigationController
+        // navigationController?.pushViewController(tela02 ?? UIViewController(), animated: true)    }
+    }
+    
+}
